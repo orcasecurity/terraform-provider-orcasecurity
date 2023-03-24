a69546d0f99046952872eefd5c73417fd52f6f90
alex.oleshkevich <alex.oleshkevich@gmail.com>
2023-03-24 17:25:21 +0100

    implement rbac group update resource

--- examples/main.tf
+++ examples/main.tf
@@ -12,11 +12,15 @@ data "orcasecurity_rbac_group" "example" {}
 
 resource "orcasecurity_rbac_group" "admin_group" {
   name        = "Admin"
-  description = "This group is for admins"
+  description = "This group is for admins."
   sso_group   = true
 }
 
 
 output "orca_rbac_groups" {
   value = data.orcasecurity_rbac_group.example
+}
+
+output "admin_group_id" {
+  value = orcasecurity_rbac_group.admin_group.id
 }
