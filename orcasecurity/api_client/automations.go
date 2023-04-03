fd3403bcac55d110d07a0029038979d7d0692503
alex.oleshkevich <alex.oleshkevich@gmail.com>
2023-04-03 14:09:22 +0200

    wis-358 skeleton for automations resource

---
+++ orcasecurity/api_client/automations.go
@@ -0,0 +1,109 @@
+package api_client
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type Automation struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type automationAPIResponseType struct {
+	Data Automation `json:"data"`
+}
+
+func (client *APIClient) GetAutomation(automationID string) (*Automation, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/rules/%s", client.APIEndpoint, automationID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := client.doRequest(*req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := automationAPIResponseType{}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response.Data, nil
+}
+
+func (client *APIClient) CreateAutomation(automation Automation) (*Automation, error) {
+	payload, err := json.Marshal(automation)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(
+		"POST",
+		fmt.Sprintf("%s/api/rules", client.APIEndpoint),
+		strings.NewReader(string(payload)),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := client.doRequest(*req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := automationAPIResponseType{}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response.Data, nil
+
+}
+
+func (client *APIClient) UpdateAutomation(ID string, data Automation) (*Automation, error) {
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(
+		"PUT",
+		fmt.Sprintf("%s/api/rules/%s", client.APIEndpoint, ID),
+		strings.NewReader(string(payload)),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := client.doRequest(*req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := automationAPIResponseType{}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response.Data, nil
+}
+
+func (client *APIClient) DeleteAutomation(ID string) error {
+	req, err := http.NewRequest(
+		"DELETE",
+		fmt.Sprintf("%s/api/rules/%s", client.APIEndpoint, ID),
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = client.doRequest(*req)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ orcasecurity/automations_resource.go
@@ -0,0 +1,190 @@
+package orcasecurity
+
+import (
+	"context"
+	"fmt"
+	"terraform-provider-orcasecurity/orcasecurity/api_client"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var (
+	_ resource.Resource                = &automationResource{}
+	_ resource.ResourceWithConfigure   = &automationResource{}
+	_ resource.ResourceWithImportState = &automationResource{}
+)
+
+type automationResource struct {
+	apiClient *api_client.APIClient
+}
+
+type automationModel struct {
+	ID          types.String `tfsdk:"id"`
+	Name        types.String `tfsdk:"name"`
+	Description types.String `tfsdk:"description"`
+}
+
+func NewAutomationResource() resource.Resource {
+	return &automationResource{}
+}
+
+func (r *automationResource) Metadata(_ context.Context, req resource.MetadataRequest, res *resource.MetadataResponse) {
+	res.TypeName = req.ProviderTypeName + "_automation"
+}
+
+func (r *automationResource) Configure(_ context.Context, req resource.ConfigureRequest, res *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+	r.apiClient = req.ProviderData.(*api_client.APIClient)
+}
+
+func (r *automationResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
+
+func (r *automationResource) Schema(_ context.Context, req resource.SchemaRequest, res *resource.SchemaResponse) {
+	res.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+				Description: "Automation ID",
+			},
+			"name": schema.StringAttribute{
+				Description: "Automation name",
+				Required:    true,
+			},
+			"description": schema.StringAttribute{
+				Description: "Automation description",
+				Optional:    true,
+			},
+		},
+	}
+}
+
+func (r *automationResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan automationModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	instance, err := r.apiClient.CreateAutomation(api_client.Automation{
+		Name:        plan.Name.ValueString(),
+		Description: plan.Description.ValueString(),
+	})
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error creating Automation",
+			"Could not create Automation, unexpected error: "+err.Error(),
+		)
+	}
+
+	plan.ID = types.StringValue(instance.ID)
+	diags = resp.State.Set(ctx, plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *automationResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var state automationModel
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	instance, err := r.apiClient.GetAutomation(state.ID.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error reading Automation",
+			fmt.Sprintf("Could not read Automation ID %s: %s", state.Name.ValueString(), err.Error()),
+		)
+	}
+	state.ID = types.StringValue(instance.ID)
+	state.Name = types.StringValue(instance.Name)
+	state.Description = types.StringValue(instance.Description)
+
+	diags = req.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *automationResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan automationModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if plan.ID.ValueString() == "" {
+		resp.Diagnostics.AddError(
+			"Plan ID is null",
+			"Could not update Automation, unexpected error: "+plan.ID.ValueString(),
+		)
+		return
+	}
+
+	_, err := r.apiClient.UpdateAutomation(
+		plan.ID.ValueString(),
+		api_client.Automation{
+			Name:        plan.Name.ValueString(),
+			Description: plan.Description.ValueString(),
+		},
+	)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error updating Automation",
+			"Could not update Automation, unexpected error: "+err.Error(),
+		)
+		return
+	}
+
+	instance, err := r.apiClient.GetAutomation(plan.ID.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error reading Automation",
+			"Could not read Automation ID: "+plan.ID.ValueString()+": "+err.Error(),
+		)
+	}
+	plan.Name = types.StringValue(instance.Name)
+	plan.Description = types.StringValue(instance.Description)
+
+	diags = resp.State.Set(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *automationResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var state automationModel
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	err := r.apiClient.DeleteAutomation(state.ID.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error deleting Automation",
+			"Could not delete Automation, unexpected error: "+err.Error(),
+		)
+		return
+	}
+}
