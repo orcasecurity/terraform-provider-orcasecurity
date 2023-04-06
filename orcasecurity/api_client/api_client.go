1c6381423494a341cc00d1395317164428aecfc6
alex.oleshkevich <alexo@orca.security>
2023-04-06 14:02:01 +0200

    test api client

--- orcasecurity/api_client/api_client.go
+++ orcasecurity/api_client/api_client.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -24,31 +25,43 @@ func NewAPIClient(endpoint, token *string) (*APIClient, error) {
 	return &apiclient, nil
 }
 
+// Convenience wrapper over http.Response
 type APIResponse struct {
 	_body    []byte
 	response *http.Response
 }
 
+// Return response status code
 func (resp *APIResponse) StatusCode() int {
 	return resp.response.StatusCode
 }
 
+// Test if request was successfull (<300)
 func (resp *APIResponse) IsOk() bool {
-	return resp.StatusCode() == http.StatusOK
+	return resp.StatusCode() < 300
 }
 
+// Read response body.
+// Careful, it contains full body in the memory.
+// If you wish a memory-effective version then use Execute function
+// that returns pointer to raw http.Response.
 func (resp *APIResponse) Body() []byte {
 	return resp._body
 }
 
+// Returns either response body or response error.
+// Note, the error contains the message provided by API on unsuccessful request.
 func (resp *APIResponse) Read() ([]byte, error) {
 	return resp.Body(), resp.Error()
 }
 
+// Load response JSON into user struct.
 func (resp *APIResponse) ReadJSON(typ interface{}) error {
 	return json.Unmarshal(resp.Body(), typ)
 }
 
+// Return API error message.
+// Returns nil if request was successful.
 func (resp *APIResponse) Error() error {
 	type errorType struct {
 		Message string `json:"message,omitempty"`
@@ -69,11 +82,16 @@ func (resp *APIResponse) Error() error {
 	return nil
 }
 
-func (c *APIClient) doRequest(req http.Request) (*APIResponse, error) {
+// Perform API call.
+func (c *APIClient) Execute(req http.Request) (*http.Response, error) {
 	req.Header.Set("authorization", fmt.Sprintf("Token %s", c.APIToken))
 	req.Header.Set("content-type", "application/json")
 	req.Header.Set("user-agent", "orca-terraform-provider (+https://registry.terraform.io/providers/orcasecurity)")
-	res, err := c.HTTPClient.Do(&req)
+	return c.HTTPClient.Do(&req)
+}
+
+func (c *APIClient) doRequest(req http.Request) (*APIResponse, error) {
+	res, err := c.Execute(req)
 	if err != nil {
 		return nil, err
 	}
@@ -90,11 +108,74 @@ func (c *APIClient) doRequest(req http.Request) (*APIResponse, error) {
 	}, nil
 }
 
+// Execute GET HTTP request.
 func (c *APIClient) Get(path string) (*APIResponse, error) {
-	fullUrl := fmt.Sprintf("%s%s", c.APIEndpoint, path)
-	req, err := http.NewRequest("GET", fullUrl, nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s%s", c.APIEndpoint, path), nil)
 	if err != nil {
 		return nil, err
 	}
 	return c.doRequest(*req)
+}
+
+// Execute POST HTTP request.
+func (c *APIClient) Post(path string, data interface{}) (*APIResponse, error) {
+	payload, err := json.Marshal(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(
+		"POST",
+		fmt.Sprintf("%s%s", c.APIEndpoint, path),
+		strings.NewReader(string(payload)),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.doRequest(*req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// Execute PUT HTTP request.
+func (c *APIClient) Put(path string, data interface{}) (*APIResponse, error) {
+	payload, err := json.Marshal(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(
+		"PUT",
+		fmt.Sprintf("%s%s", c.APIEndpoint, path),
+		strings.NewReader(string(payload)),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.doRequest(*req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// Execute DELETE HTTP request.
+func (c *APIClient) Delete(path string) (*APIResponse, error) {
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s%s", c.APIEndpoint, path), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.doRequest(*req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
