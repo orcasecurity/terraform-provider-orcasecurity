3a198d8f0755dfb83a44155d8f5f3c8e8565c1bd
alex.oleshkevich <alexo@orca.security>
2023-04-13 15:03:13 +0200

    doc updates

--- examples/resources/orcasecurity_automation/resource.tf
+++ examples/resources/orcasecurity_automation/resource.tf
@@ -2,8 +2,15 @@
 resource "orcasecurity_automation" "example" {
   name        = "JIRA issues"
   description = "Automatically create JIRA issues"
+  query = {
+    filter : [
+      { field : "state.status", includes : ["open"] },
+      { field : "state.risk_level", includes : ["high", "critical"] },
+      { field : "asset_regions", excludes : ["centralus"] },
+    ]
+  }
   jira_issue = {
     template_name = "My Template"
-    parent_issue  = "ABC-007" // optional
+    parent_issue  = "JMB-007" // optional
   }
 }
