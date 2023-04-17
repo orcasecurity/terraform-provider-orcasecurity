cf2450ef013848d4e296800b48f21ef642c18d46
alex.oleshkevich <alexo@orca.security>
2023-04-17 16:20:29 +0200

    update docs

--- examples/resources/orcasecurity_automation/resource.tf
+++ examples/resources/orcasecurity_automation/resource.tf
@@ -14,3 +14,19 @@ resource "orcasecurity_automation" "example" {
     parent_issue  = "JMB-007" // optional
   }
 }
+
+// usage witn Sumo Logic integration
+resource "orcasecurity_automation" "example" {
+  name        = "JIRA issues"
+  description = "Automatically create JIRA issues"
+  query = {
+    filter : [
+      { field : "state.status", includes : ["open"] },
+      { field : "state.risk_level", includes : ["high", "critical"] },
+      { field : "asset_regions", excludes : ["centralus"] },
+    ]
+  }
+  sumologic = {
+
+  }
+}
