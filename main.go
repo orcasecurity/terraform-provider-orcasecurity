cfd5e9877bf4b2986c73511ba442d5bffe84188d
Alex Oleshkevich <90266512+alexo-orca@users.noreply.github.com>
2023-05-05 16:44:14 +0200

    integrate with sentry (#9)

--- main.go
+++ main.go
@@ -10,8 +10,12 @@ import (
 
 	"terraform-provider-orcasecurity/orcasecurity"
 
+	"github.com/getsentry/sentry-go"
+
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
+
+const sentryDSN = "https://6a305d50bdb04e0aa709c8f2f24dec03@o482658.ingest.sentry.io/4505086124752896"
 
 var (
 	// these will be set by the goreleaser configuration
@@ -28,12 +32,20 @@ func main() {
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:   sentryDSN,
+		Debug: debug,
+	})
+	if err != nil {
+		log.Printf("Sentry.Init failure: %s", err)
+	}
+
 	opts := providerserver.ServeOpts{
 		Address: "registry.terraform.io/orcasecurity/orcasecurity",
 		Debug:   debug,
 	}
 
-	err := providerserver.Serve(context.Background(), orcasecurity.New(version), opts)
+	err = providerserver.Serve(context.Background(), orcasecurity.New(version), opts)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
