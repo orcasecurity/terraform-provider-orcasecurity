2775b3ddc6b62f302d02abc8a99234e0708beb3c
Patrick Burke <patrick.burke@Patricks-MacBook-Pro-2.local>
2024-08-07 11:14:23 -0500

    2nd PR

--- orcasecurity/api_client/automations_test.go
+++ orcasecurity/api_client/automations_test.go
@@ -1,24 +1,24 @@
 package api_client_test
 
 import (
-	"io/ioutil"
+	"io"
 	"net/http"
 	"strings"
 	"terraform-provider-orcasecurity/orcasecurity/api_client"
 	"testing"
 )
 
-func TestAutomations_IsAutomationExists(t *testing.T) {
+func TestAutomations_DoesAutomationExist(t *testing.T) {
 	httpClient := &http.Client{Transport: api_client.RoundTripFunc(func(req *http.Request) *http.Response {
 		return &http.Response{
 			StatusCode: 200,
-			Body:       ioutil.NopCloser(strings.NewReader(`ok`)),
+			Body:       io.NopCloser(strings.NewReader(`ok`)),
 			Request:    req,
 		}
 	})}
 
 	apiClient := api_client.APIClient{APIEndpoint: "http://localhost", APIToken: "secret", HTTPClient: httpClient}
-	exists, err := apiClient.IsAutomationExists("1")
+	exists, err := apiClient.DoesAutomationExist("1")
 	if err != nil {
 		t.Error(err)
 	}
@@ -27,22 +27,21 @@ func TestAutomations_IsAutomationExists(t *testing.T) {
 	}
 
 }
-func TestAutomations_IsAutomationExistsFalse(t *testing.T) {
+func TestAutomations_DoesAutomationExistFalse(t *testing.T) {
 	httpClient := &http.Client{Transport: api_client.RoundTripFunc(func(req *http.Request) *http.Response {
 		return &http.Response{
 			StatusCode: 404,
-			Body:       ioutil.NopCloser(strings.NewReader(`ok`)),
+			Body:       io.NopCloser(strings.NewReader(`ok`)),
 			Request:    req,
 		}
 	})}
 
 	apiClient := api_client.APIClient{APIEndpoint: "http://localhost", APIToken: "secret", HTTPClient: httpClient}
-	exists, err := apiClient.IsAutomationExists("1")
+	exists, err := apiClient.DoesAutomationExist("1")
 	if err != nil {
 		t.Error(err)
 	}
 	if exists {
 		t.Error("automation expected to be absent, but it exists")
 	}
-
 }
--- orcasecurity/webhooks/data_source_test.go
+++ orcasecurity/webhooks/data_source_test.go
@@ -34,4 +34,5 @@ func TestAccWebhookDataSource(t *testing.T) {
 			},
 		},
 	})
+
 }
