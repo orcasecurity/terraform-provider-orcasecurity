02b9ff164d0c4be6261c9cb441b70f407c4beb2d
patrickb-orca <patrick.burke@orca.security>
2024-09-07 14:08:02 -0500

    Just ran go generate ./... per recommendation

--- examples/resources/orcasecurity_group/resource.tf
+++ examples/resources/orcasecurity_group/resource.tf
@@ -1,10 +1,10 @@
 //Group
 resource "orcasecurity_group" "tf-group-1" {
-    name = "Orca Terraform Group 1"
-    
-    sso_group = true
-    description = "string"
-    users = [
-        "{place-user-id-here}"
-    ]
+  name = "Orca Terraform Group 1"
+
+  sso_group   = true
+  description = "string"
+  users = [
+    "{place-user-id-here}"
+  ]
 }
