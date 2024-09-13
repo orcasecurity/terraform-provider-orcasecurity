665314f69d0b987b06de6ea4f3210949b17af651
patrickjburke245 <24pburke@gmail.com>
2024-09-13 08:06:53 -0500

    Go Generated

--- examples/resources/orcasecurity_discovery_view/resource.tf
+++ examples/resources/orcasecurity_discovery_view/resource.tf
@@ -1,16 +1,16 @@
 //Saved discovery view
 resource "orcasecurity_discovery_view" "tf-disco-view-1" {
-    name = "orca-disco-view-1"
+  name = "orca-disco-view-1"
 
-    organization_level = true
-    view_type = "discovery"
-    extra_params = {}
-    filter_data = {
-        query = jsonencode({
-            "models": [
-                "AwsS3Bucket"
-            ],
-            "type": "object_set"
-        })
-    }
+  organization_level = true
+  view_type          = "discovery"
+  extra_params       = {}
+  filter_data = {
+    query = jsonencode({
+      "models" : [
+        "AwsS3Bucket"
+      ],
+      "type" : "object_set"
+    })
+  }
 }
