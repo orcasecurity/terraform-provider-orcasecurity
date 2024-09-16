b436516b9bb00140a21e853f0677bd92563b3516
patrickb-orca <patrick.burke@orca.security>
2024-09-15 22:47:26 -0500

    5th commit

--- examples/resources/orcasecurity_business_unit/resource.tf
+++ examples/resources/orcasecurity_business_unit/resource.tf
@@ -1,7 +1,7 @@
 //Cloud Provider-based Business unit for AWS
 resource "orcasecurity_business_unit" "business_unit_for_aws" {
-    name = "AWS"
-    filter_data = {
-        cloud_provider = ["aws"]
-    }
+  name = "AWS"
+  filter_data = {
+    cloud_provider = ["aws"]
+  }
 }
