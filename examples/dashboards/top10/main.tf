a6d296e0bdc40e4425102d0dc10ca577d3c23b22
junninho-orca <junninho.thomas@orca.security>
2025-01-30 11:05:47 -0600

    add example dashboards (#122)
    * add example dashboards
    
    * fix build errors
    
    ---------
    
    Co-authored-by: junninho <junninhothomas1998@gmail.com>

---
+++ examples/dashboards/top10/main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    orcasecurity = {
+      source = "orcasecurity/orcasecurity"
+    }
+  }
+}
+
+provider "orcasecurity" {
+  api_endpoint = "https://api.orcasecurity.io"
+  api_token    = var.api_token
+}
+
+module "top10_widgets" {
+  source = "./modules"
+}
+
+resource "orcasecurity_custom_dashboard" "security_category_dashboard" {
+  name               = "Top 10 Category Dashboard"
+  organization_level = true
+  filter_data        = {}
+  view_type          = "dashboard"
+  extra_params = {
+    description = "Top 10 alerts from all major alert categories"
+    widgets_config = [
+      {
+        id   = module.top10_widgets.malware_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.vulnerability_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.data_risk_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.iam_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.authentication_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.lateral_movement_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.suspicious_activity_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.malicious_activity_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.neglected_assets_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.network_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.authentication_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.ai_security_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.api_security_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.data_protection_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.file_integrity_alerts_widget_id
+        size = "sm"
+      },
+      {
+        id   = module.top10_widgets.logging_monitoring_alerts_widget_id
+        size = "sm"
+      }
+    ]
+  }
+}
+
