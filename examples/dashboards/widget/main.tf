929693f0e7191fe673ffa257acf79dd115889efc
Koby Kagan <koby@orca.security>
2026-02-11 19:53:22 +0200

    WASP-1012\add more documentation and examples for dashboard and widgets to terraform provider

---
+++ examples/dashboards/widget/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    orcasecurity = {
+      source = "orcasecurity/orcasecurity"
+    }
+  }
+}
+
+provider "orcasecurity" {
+  api_endpoint = var.api_endpoint
+  api_token    = var.api_token
+}
+
+module "widget_builtins" {
+  source = "./modules"
+}
+
+resource "orcasecurity_custom_dashboard" "widget_test" {
+  name               = "Widget ID Dashboard"
+  organization_level = true
+  filter_data        = {}
+  view_type          = "dashboard"
+  extra_params = {
+    description   = "Dashboard with built-in widget IDs"
+    widgets_config = module.widget_builtins.widgets_config
+  }
+}
+
+output "dashboard_id" {
+  value = orcasecurity_custom_dashboard.widget_test.id
+}
