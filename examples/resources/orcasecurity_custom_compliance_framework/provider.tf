c2e613af0981affa3fd102657966857a800d7be4
igorlopes-orca <igor.lopes@orca.security>
2026-03-30 17:21:14 -0300

    chore: delete test resources, keep only example resource
    Signed-off-by: igorlopes-orca <igor.lopes@orca.security>

--- examples/resources/orcasecurity_custom_compliance_framework/provider.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    orcasecurity = {
-      source = "orcasecurity/orcasecurity"
-    }
-  }
-}
-
-provider "orcasecurity" {}
--- examples/resources/orcasecurity_custom_compliance_framework/resource.tf
+++ examples/resources/orcasecurity_custom_compliance_framework/resource.tf
@@ -1,5 +1,5 @@
 resource "orcasecurity_custom_compliance_framework" "example" {
-  name        = "My Custom Framework - Igor tests"
+  name        = "My Custom Framework"
   description = "Custom compliance framework managed by Terraform"
 
   sections = [
@@ -13,6 +13,15 @@ resource "orcasecurity_custom_compliance_framework" "example" {
         {
           rule_id              = "r8ae477067a"
           rule_id_in_framework = "1.2"
+        }
+      ]
+    },
+    {
+      name = "Data Protection"
+      tests = [
+        {
+          rule_id              = "r8ae477067a"
+          rule_id_in_framework = "2.1"
         }
       ]
     }
